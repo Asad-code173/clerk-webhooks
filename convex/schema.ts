669991154de8a1e9f5a1c1a9b3d7df02a5165104
Asad-code173 <asadfarzand711@gmail.com>
2025-06-10 00:06:43 -0700

    clerk-webhooks

---
+++ convex/schema.ts
@@ -0,0 +1,19 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  registeration: defineTable({
+    clerkId: v.string(),
+    email: v.string(),
+    role: v.string(),
+  }),
+  flats: defineTable({
+  flatNumber: v.string(),
+  flatSize: v.string(), // or number, e.g. in sqft
+  floor: v.optional(v.number()),
+  status: v.union(v.literal("occupied"), v.literal("vacant")),
+  residentClerkId: v.optional(v.string()), // if occupied
+}),
+
+});
+
